2c0973ac0f63a194713b4d54b9ae46b5d32e2d0f
sackboy97 <michele.ripoli@hotmail.it>
2017-12-22 23:19:56 +0100

    test

---
+++ new_python_app/receiver.py
@@ -0,0 +1,16 @@
+"""
+Receiver
+"""
+import socket
+
+UDP_IP = "192.168.1.1"
+UDP_PORT = 5000
+print(UDP_IP, UDP_PORT)
+SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+SOCK.bind((UDP_IP, UDP_PORT))
+
+while True:
+    DATA = SOCK.recvfrom(1024)
+    print("Message:", DATA)
+    if DATA:
+        print("Receiving...")
