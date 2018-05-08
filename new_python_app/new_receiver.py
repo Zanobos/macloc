1e50f551c1546859254eba8d892521fade526b20
sackboy97 <michele.ripoli@hotmail.it>
2018-05-08 19:25:15 +0200

    Sistemato davvero programma per ricezione su Pi

---
+++ new_python_app/new_receiver.py
@@ -0,0 +1,21 @@
+"""
+Receiver
+"""
+import socket
+import struct
+
+SOCK = socket.socket(socket.PF_CAN, socket.SOCK_RAW, socket.CAN_RAW)
+SOCK.bind(("can0",))
+
+fmt = "<IB3x8s"
+
+while 1:
+	can_pkt = SOCK.recv(16)
+	can_id, length, data = struct.unpack(fmt, can_pkt)
+	can_id &= socket.CAN_EFF_MASK
+
+	print("ID:", format(can_id, '03b'), end="")
+	print("\t\tData: ", end="")
+	for i in range(length):
+		print("", format(data[i], '02X'), end="")
+	print("")
---
+++ new_python_app/pc_client.py
@@ -0,0 +1,19 @@
+"""
+Client
+"""
+import socket
+
+UDP_IP = "192.168.1.1"
+UDP_PORT = 5000
+MESSAGE = "Test"
+ADDR = (UDP_IP, UDP_PORT)
+
+print("UDP target IP:", UDP_IP)
+print("UDP target port:", UDP_PORT)
+print("Message:", MESSAGE)
+
+SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+for i in range(2):
+	SOCK.sendto(MESSAGE.encode(), ADDR)
+	if SOCK.sendto(MESSAGE.encode(), ADDR):
+		print("Sending...")
