b2374c57acdd2a98d233b75dc3dfeb1cd6acf765
Zanotti Andrea <a.zanotti@reply.it>
2018-08-19 20:11:00 +0200

    fixed walls table

--- app/models.py
+++ app/models.py
@@ -3,7 +3,15 @@ from app import db
 class Wall(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     height = db.Column(db.Float)
-    weight = db.Column(db.Float)
+    width = db.Column(db.Float)
 
     def __repr__(self):
         return '<Wall {}, h={}, w={}>' .format(self.id, self.height, self.weight)
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'height': self.height,
+            'width': self.width
+        }
+        return data
--- migrations/versions/59c9624d1730_walls_table.py
+++ migrations/versions/59c9624d1730_walls_table.py
@@ -1,8 +1,8 @@
 """walls table
 
-Revision ID: a3e1ccdca3b9
+Revision ID: 59c9624d1730
 Revises: 
-Create Date: 2018-08-19 19:53:34.627413
+Create Date: 2018-08-19 20:10:15.947706
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a3e1ccdca3b9'
+revision = '59c9624d1730'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -21,7 +21,7 @@ def upgrade():
     op.create_table('wall',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('height', sa.Float(), nullable=True),
-    sa.Column('weight', sa.Float(), nullable=True),
+    sa.Column('width', sa.Float(), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
