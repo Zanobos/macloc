02efde847dfab5ff1afa16a25d6b847f957ef580
Zanotti Andrea <a.zanotti@reply.it>
2018-08-20 16:50:08 +0200

    Added start of User and Climb

--- app/models.py
+++ app/models.py
@@ -27,7 +27,7 @@ class Wall(PaginatedAPIMixin, db.Model):
     width = db.Column(db.Float)
 
     def __repr__(self):
-        return '<Wall {}, h={}, w={}>' .format(self.id, self.height, self.weight)
+        return '<Wall {}, h={}, w={}>' .format(self.id, self.height, self.width)
 
     def to_dict(self):
         data = {
@@ -39,4 +39,52 @@ class Wall(PaginatedAPIMixin, db.Model):
 
     def from_dict(self, data):
         for field in ['height', 'width']:
+            setattr(self, field, data[field])
+
+class User(PaginatedAPIMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    nickname = db.Column(db.String(20), index=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    height = db.Column(db.Float)
+    weight = db.Column(db.Float)
+    climbs = db.relationship('Climb', backref='climber', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {} ({})>'.format(self.name, self.nickname)
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'name': self.name,
+            'nickname': self.nickname,
+            'email': self.email,
+            'height': self.height,
+            'weight': self.weight
+        }
+        return data
+
+    def from_dict(self, data):
+        for field in ['name', 'nickname', 'email', 'height', 'weight']:
+            setattr(self, field, data[field])
+
+#Remember, user can be referenced with relationship, but
+# holds and walls must be copied because they can change
+class Climb(PaginatedAPIMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    grade = db.Column(db.Float)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Climb {}, grade={}>'.format(self.id, self.grade)
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'grade': self.grade
+        }
+        return data
+
+    def from_dict(self, data):
+        for field in ['grade']:
             setattr(self, field, data[field])
