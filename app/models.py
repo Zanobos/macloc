2814c91c9accdd6469355aec2aeaab8e28732605
Zanotti Andrea <a.zanotti@reply.it>
2018-08-20 20:05:37 +0200

    added records table

--- app/models.py
+++ app/models.py
@@ -23,26 +23,6 @@ class PaginatedAPIMixin(object):
         }
         return data
 
-class Wall(PaginatedAPIMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    height = db.Column(db.Float)
-    width = db.Column(db.Float)
-    holds = db.relationship('Hold', backref='mounted_on', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Wall {}, h={}, w={}>' .format(self.id, self.height, self.width)
-
-    def to_dict(self):
-        data = {
-            'id': self.id,
-            'height': self.height,
-            'width': self.width
-        }
-        return data
-
-    def from_dict(self, data):
-        for field in ['height', 'width']:
-            setattr(self, field, data[field])
 
 class User(PaginatedAPIMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -71,12 +51,14 @@ class User(PaginatedAPIMixin, db.Model):
         for field in ['name', 'nickname', 'email', 'height', 'weight']:
             setattr(self, field, data[field])
 
-#Remember, user can be referenced with relationship, but
+# Remember, user can be referenced with relationship, but
 # holds and walls must be copied because they can change
+# For this reason I store wall information as attributes
 class Climb(PaginatedAPIMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     grade = db.Column(db.Float)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    historic_wall = db.relationship('HistoricWall', uselist=False, backref='used_on', lazy='dynamic')
 
     def __repr__(self):
         return '<Climb {}, grade={}>'.format(self.id, self.grade)
@@ -92,20 +74,43 @@ class Climb(PaginatedAPIMixin, db.Model):
         for field in ['grade']:
             setattr(self, field, data[field])
 
+class Wall(PaginatedAPIMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    height = db.Column(db.Float)
+    width = db.Column(db.Float)
+    holds = db.relationship('Hold', backref='mounted_on', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Wall {}, h={}, w={}>' .format(self.id, self.height, self.width)
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'height': self.height,
+            'width': self.width
+        }
+        return data
+
+    def from_dict(self, data):
+        for field in ['height', 'width']:
+            setattr(self, field, data[field])
+            
 class Hold(PaginatedAPIMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    can_id = db.Column(db.Integer)
     dist_from_sx = db.Column(db.Float)
     dist_from_bot = db.Column(db.Float)
     holdType = db.Column(db.String(30))
     wall_id = db.Column(db.Integer, db.ForeignKey('wall.id'))
 
     def __repr__(self):
-        return '<Hold {} ({},{}) t={}>'.format(self.id, self.dist_from_sx, \
+        return '<Hold {}, can_id={} ({},{}) t={}>'.format(self.id, self.can_id, self.dist_from_sx, \
                 self.dist_from_bot, self.holdType)
 
     def to_dict(self):
         data = {
             'id': self.id,
+            'can_id': self.can_id,
             'dist_from_sx': self.dist_from_sx,
             'dist_from_bot': self.dist_from_bot,
             'holdType': self.holdType
@@ -115,3 +120,64 @@ class Hold(PaginatedAPIMixin, db.Model):
     def from_dict(self, data):
         for field in ['dist_from_sx', 'dist_from_bot', 'holdType']:
             setattr(self, field, data[field])
+
+class HistoricWall(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    height = db.Column(db.Float)
+    width = db.Column(db.Float)
+    holds = db.relationship('HistoricHold', backref='mounted_on', lazy='dynamic')
+    wall_id = db.Column(db.Integer, db.ForeignKey('historic_wall.id'))
+
+    def __repr__(self):
+        return '<HistoricWall {}, h={}, w={}>' .format(self.id, self.height, self.width)
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'height': self.height,
+            'width': self.width
+        }
+        return data
+
+class HistoricHold(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    can_id = db.Column(db.Integer)
+    dist_from_sx = db.Column(db.Float)
+    dist_from_bot = db.Column(db.Float)
+    holdType = db.Column(db.String(30))
+    wall_id = db.Column(db.Integer, db.ForeignKey('historic_wall.id'))
+    records = db.relationship('Record', backref='of_hold', lazy='dynamic')
+
+    def __repr__(self):
+        return '<HistoricHold {}, can_id={} ({},{}) t={}>'.format(self.id, self.can_id, \
+                self.dist_from_sx, self.dist_from_bot, self.holdType)
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'can_id': self.can_id,
+            'dist_from_sx': self.dist_from_sx,
+            'dist_from_bot': self.dist_from_bot,
+            'holdType': self.holdType
+        }
+        return data
+
+class Record(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    can_id = db.Column(db.Integer)
+    #missing the three forces
+    hold_id = db.Column(db.Integer, db.ForeignKey('historic_hold.id'))
+
+    def __repr__(self):
+        return '<Record {}, can_id={}>'.format(self.id, self.can_id)
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'can_id': self.can_id,
+            'hold_id': self.hold_id
+        }
+        return data
+
+    def to_ws_dict(self):
+        pass
