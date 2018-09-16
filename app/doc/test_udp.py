e31b0d17e7a56cd1783f79df118227aa5e5186cb
Zanotti Andrea <a.zanotti@reply.it>
2018-09-16 14:43:05 +0200

    stream to FE working

--- app/doc/test_udp.py
+++ app/doc/test_udp.py
@@ -5,7 +5,7 @@ UDP_IP = "127.0.0.1"
 UDP_PORT = 6000
 
 FMT = "<IB3x3h1H"
-MESSAGE = struct.pack(FMT, 41, 58, 67, 77, 87, 1200)
+MESSAGE = struct.pack(FMT, 41, 100, 100, 77, 87, 1200)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
