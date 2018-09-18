4bac899a9fb5bc9c48d7e86c61a82356edf7214a
Zanotti Andrea <a.zanotti@reply.it>
2018-09-18 10:39:01 +0200

    loop of test udp

--- app/doc/test_udp.py
+++ app/doc/test_udp.py
@@ -1,11 +1,14 @@
 import socket
 import struct
+import time
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 6000
 
 FMT = "<IB3x3h1H"
-MESSAGE = struct.pack(FMT, 41, 100, 100, 77, 87, 1200)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+for i in range(0, 200):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    MESSAGE = struct.pack(FMT, 41, 100, 100, 77, i, 1200)
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    time.sleep(.01)
