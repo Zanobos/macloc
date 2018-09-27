4a8ae8fe8286f68945391d03f89d831af6459a8b
Zanotti Andrea <a.zanotti@reply.it>
2018-09-27 09:22:06 +0200

    reworked store

--- webapp/src/store/index.js
+++ webapp/src/store/index.js
@@ -1,21 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import walls from './modules/walls'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    count: 0,
-    inProgress: false
-  },
-  mutations: {
-    increment (state) {
-      state.count++
-    }
-  },
-  getters: {
-    isInProgress: state => {
-      return state.inProgress
-    }
+  modules: {
+    walls
   }
 })
---
+++ webapp/src/store/modules/walls.js
@@ -0,0 +1,25 @@
+const state = {
+  inProgress: false
+}
+
+const getters = {
+  isInProgress: state => {
+    return state.inProgress
+  }
+}
+
+const actions = {
+
+}
+
+const mutations = {
+
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
