fac37d9f17a09108b3e6973ef21288e4b4e49bd8
Zanotti Andrea <a.zanotti@reply.it>
2018-09-28 11:54:05 +0200

    added all walls api

--- webapp/src/api/modules/apiwalls.js
+++ webapp/src/api/modules/apiwalls.js
@@ -14,5 +14,33 @@ export default {
         (response) => cb(response),
         (error) => ecb(error)
       )
+  },
+  postWalls (wall, cb, ecb) {
+    HTTP.post('walls', wall)
+      .then(
+        (response) => cb(response),
+        (error) => ecb(error)
+      )
+  },
+  putWall (wall, cb, ecb) {
+    HTTP.put('walls/' + wall.id, wall)
+      .then(
+        (response) => cb(response),
+        (error) => ecb(error)
+      )
+  },
+  patchWall (wall, cb, ecb) {
+    HTTP.patch('walls/' + wall.id, wall)
+      .then(
+        (response) => cb(response),
+        (error) => ecb(error)
+      )
+  },
+  deleteWall (id, cb, ecb) {
+    HTTP.delete('walls/' + id)
+      .then(
+        (response) => cb(response),
+        (error) => ecb(error)
+      )
   }
 }
