eaa155c38c206822a6639fdb56bef79f0e39a7d8
Zanotti Andrea <a.zanotti@reply.it>
2018-10-03 17:18:25 +0200

    init climbers page

--- webapp/src/components/pages/ClimbsHistory.vue
+++ webapp/src/components/pages/ClimbsHistory.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     QUI TUTTE LE SCALATE FATTE
+    <ul>
+      <li
+        v-for="climb in climbs"
+        :key="climb.id">
+        Climber: {{ climb.climber_name }}
+        Date: {{climb.start_time}}
+      </li>
+    </ul>
+    <b-pagination v-on:change="onPageChange" align="center" :total-rows="this.climbsMeta.total_items" v-model="climbsMeta.page" :per-page="climbsMeta.per_page">
+    </b-pagination>
+
   </div>
 </template>
 
@@ -10,10 +21,19 @@ import { mapState } from 'vuex'
 export default {
   name: 'ClimbsHistory',
   computed: mapState({
-    climbs: state => state.climbs.all
+    climbs: state => state.climbs.climbs,
+    climbsMeta: state => state.climbs.climbsMeta
   }),
+  methods: {
+    onPageChange: function (page) {
+      this.climbsMeta.page = page
+      this.$store.dispatch('climbs/fetchClimbs', this.climbsMeta)
+    }
+  },
   created () {
-    this.$store.dispatch('climbs/fetchClimbs')
+    this.$store.dispatch('climbs/initClimbsMeta', {page: 1, per_page: 1}).then(() => {
+      this.$store.dispatch('climbs/fetchClimbs', this.climbsMeta)
+    })
   }
 }
 </script>
--- webapp/src/store/modules/climbs.js
+++ webapp/src/store/modules/climbs.js
@@ -1,25 +1,36 @@
 import apiclimbs from '@/api/modules/apiclimbs'
+import { defaultErrorHandler } from '@/api'
 
 const state = {
-  climbs: []
+  climbs: [],
+  climbsMeta: {}
 }
 
 const getters = {}
 
 const actions = {
-  fetchClimbs ({ commit }) {
+  fetchClimbs ({ commit }, payload) {
     apiclimbs.getClimbs(
       (response) => {
         commit('storeClimbs', { climbs: response.data.items })
+        commit('storeClimbsMeta', { meta: response.data._meta })
       },
-      () => {} // Default error handler?
+      (error) => defaultErrorHandler(error),
+      payload.page,
+      payload.per_page
     )
+  },
+  initClimbsMeta ({ commit }, payload) {
+    commit('storeClimbsMeta', { meta: payload })
   }
 }
 
 const mutations = {
   storeClimbs (state, { climbs }) {
     state.climbs = climbs
+  },
+  storeClimbsMeta (state, { meta }) {
+    state.climbsMeta = meta
   }
 }
 
