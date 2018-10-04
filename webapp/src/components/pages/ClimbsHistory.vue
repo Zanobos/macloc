8ce019bfbd0548cfa769a8cfe65a74cd02a3f9ee
Zanotti Andrea <a.zanotti@reply.it>
2018-10-04 09:09:37 +0200

    climbs history

--- webapp/src/components/pages/ClimbsHistory.vue
+++ webapp/src/components/pages/ClimbsHistory.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
-    QUI TUTTE LE SCALATE FATTE
-    <ul>
-      <li
-        v-for="climb in climbs"
+    <b-card no-body class="mb-1" v-for="climb in climbs"
         :key="climb.id">
-        Climber: {{ climb.climber_name }}
-        Date: {{climb.start_time}}
-      </li>
-    </ul>
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-btn block href="#" v-b-toggle.accordion2 variant="info">Climb #{{climb.id}}</b-btn>
+      </b-card-header>
+      <b-collapse id="accordion2" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <p class="card-text" style="text-align: left">
+            <ul>
+              <li>Climber: {{ climb.climber_name }} </li>
+              <li>Date: {{climb.start_time}} </li>
+              <li>Wall grade: {{climb.wall_grade}} </li>
+            </ul>
+          </p>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
     <b-pagination v-on:change="onPageChange" align="center" :total-rows="this.climbsMeta.total_items" v-model="climbsMeta.page" :per-page="climbsMeta.per_page">
     </b-pagination>
 
@@ -31,7 +40,7 @@ export default {
     }
   },
   created () {
-    this.$store.dispatch('climbs/initClimbsMeta', {page: 1, per_page: 1}).then(() => {
+    this.$store.dispatch('climbs/initClimbsMeta', {page: 1, per_page: 2}).then(() => {
       this.$store.dispatch('climbs/fetchClimbs', this.climbsMeta)
     })
   }
