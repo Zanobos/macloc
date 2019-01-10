77afbd9c0d977d94d685fd625209031a384493a1
Zanotti Andrea <a.zanotti@reply.it>
2019-01-10 15:12:12 +0100

    removed git pull from update files

--- webapp/update_fe.sh
+++ webapp/update_fe.sh
@@ -2,7 +2,6 @@
 
 #To be runned as sudo
 
-git pull                                            # Fetch the new code
 npm install
 npm run build
 cp -r dist/* /var/www/html
--- webserver/update_be.sh
+++ webserver/update_be.sh
@@ -2,7 +2,6 @@
 
 #To be runned as sudo
 
-git pull                                            # Fetch the new code
 supervisorctl stop maclocbe                         # Stop the application
 source venv/bin/activate                            # Activate the venv
 pip install -r app/doc/requirements.txt             # Install new dependencies
